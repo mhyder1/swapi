aa8fb928986a81b324ff0e7ee9f50262af6f24a7
Austin Kellum <austin_kellum@comcast.com>
2021-05-27 10:33:42 -0500

    added life cycle and fetching, props and state

--- src/components/CharacterMeta/CharacterMeta.js
+++ src/components/CharacterMeta/CharacterMeta.js
@@ -1,11 +1,13 @@
 //import './CharacterMeta.css';
 
 function CharacterMeta(props) {
+  
   return (
     <div className="character-meta-container">
-      
-        <h1>CharacterMeta</h1>
-         
+        <h1>Character</h1>
+        <p>Name: {props.CharacterMetaData.name}</p>
+        <p> Hair Color: {props.CharacterMetaData.hair_color}</p>
+        <p> Height: {props.CharacterMetaData.height}</p>  
     </div>
   );
 }
--- src/components/StarShip/StarShip.js
+++ src/components/StarShip/StarShip.js
@@ -1,11 +1,11 @@
 //import './StarShip.css';
 
-function StarShip() {
+function StarShip(props) {
   return (
     <div className="star-ship-container">
       
         <h1>StarShip</h1>
-       
+        <p>Name: { props.starshipDataProps.name}</p>
     </div>
   );
 }
